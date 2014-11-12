99d03f4a6b930b7064baac3d25841b6fab4c46bb
andreili <andreil499@gmail.com>
2014-11-12 14:58:38 +0300

    Update PCB, Starting firmware

---
+++ firmware/programm/init.h
@@ -0,0 +1,9 @@
+#ifndef _INIT_HEADER_
+#define _INIT_HEADER_
+
+#include "stm32f10x.h"
+
+void SystemInit(void);
+void SetSysClockTo72(void);
+
+#endif
---
+++ firmware/programm/main.c
@@ -0,0 +1,10 @@
+#include "stm32f10x.h"
+#include "init.h"
+
+int main()
+{
+	SystemInit();
+	while (1)
+	{
+	}
+}
